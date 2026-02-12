381aeb863ba285abc506d13983777b29aca93942
Maximguitar122 <maxym2251@gmail.com>
2026-02-12 18:14:14 +0200

    Update Program.cs

--- TrainingDbApp/Program.cs
+++ TrainingDbApp/Program.cs
@@ -4,7 +4,7 @@ namespace TrainingDbApp
 {
     public static class Program
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
             Console.WriteLine("TrainingDbApp");
         }
